27f0d5890377b6adb099af5387bdd9e77965d6f0
Gabriel Pascualy <gabrielp@ketryx.com>
2024-01-03 15:21:21 -0500

    Update page.test.tsx

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -5,7 +5,7 @@ import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
 
 describe('Home @tests:SWA-1', () => {
-  it('renders a heading', () => {
+  it('renders a better heading', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
